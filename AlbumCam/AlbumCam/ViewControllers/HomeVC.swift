edb5c8a921ea9a07d508c329f78ce83fd773df50
Chan Thai Thong <thongz0819@live.com>
2025-03-12 20:49:30 +0800

    initial commit

---
+++ AlbumCam/AlbumCam/ViewControllers/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlbumCam
+//
+//  Created by Chan Thai Thong on 12/3/25.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+    }
+
+
+}
+
